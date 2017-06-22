810e0295513ad2d3b1cf9f0f8c4a0cc415612c48
iliass <tiilias@gmail.com>
2017-06-21 18:02:34 -0700

    trained inception from scrash on cifar10

---
+++ cifar10_inception_full_traning.py
@@ -0,0 +1,261 @@
+# Load modules
+import cv2
+import csv
+import time
+import glob
+import pickle
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+import pandas as pd
+
+
+
+from keras.datasets import cifar10
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# Flatten the arrays
+y_train = y_train.reshape(-1)
+y_test = y_test.reshape(-1)
+
+# Validation data from training data
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3, random_state=42, stratify = y_train)
+
+n_train = len(y_train)
+
+n_val = len(y_val)
+
+n_test = len(y_test)
+
+image_shape = X_train[0].shape
+
+# How many unique classes/labels there are in the dataset.
+
+n_classes = len(set(y_train))
+
+img_size=X_train.shape[1] #Size of input images
+
+print("Number of training examples =", n_train)
+print("X_train shape = ", X_train.shape)
+print("Number of validation examples =", n_val)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+# Model implementation
+# Hyperparameters
+mu = 0.0
+sigma = 0.1
+base_rate = 0.0005 #Base learning rate
+dropout = 0.5 #dropout rate
+rate = base_rate
+def conv_layer(x, filter_w, in_d, out_d):
+    conv_W = tf.Variable(tf.truncated_normal(shape=(filter_w, filter_w, in_d, out_d), mean = mu, stddev = sigma))
+    conv_b = tf.Variable(tf.zeros(out_d))
+    conv_res   = tf.nn.conv2d(x, conv_W, strides=[1, 1, 1, 1], padding='SAME') + conv_b
+    return conv_res
+
+def conv_relu_layer(x, filter_w, in_d, out_d):
+    conv_W = tf.Variable(tf.truncated_normal(shape=(filter_w, filter_w, in_d, out_d), mean = mu, stddev = sigma))
+    conv_b = tf.Variable(tf.zeros(out_d))
+    conv_res   = tf.nn.conv2d(x, conv_W, strides=[1, 1, 1, 1], padding='SAME') + conv_b
+    return tf.nn.relu(conv_res)
+
+def maxpool_layer(x, filter_w):
+    return tf.nn.max_pool(x,ksize=[1,filter_w,filter_w,1], strides=[1,1,1,1],padding='SAME')
+
+def maxpool_stride_layer(x, filter_w, s):
+    return tf.nn.max_pool(x,ksize=[1,filter_w,filter_w,1], strides=[1,s,s,1],padding='VALID')
+
+def fc_layer(x, a, b):
+    fc_W = tf.Variable(tf.truncated_normal(shape=(a, b), mean = mu, stddev = sigma))
+    fc_b = tf.Variable(tf.zeros(b))
+    fc = tf.matmul(x, fc_W) + fc_b
+    return tf.nn.relu(fc)
+
+#number of outputs on the first and the second inception modules
+d_1 = 64
+d_2 = 128
+d_1x1 = 32
+
+def inception_module(x, in_d, out_d):
+    conv1x1 = conv_layer(x, 1, in_d, out_d)
+    conv2 = conv_relu_layer(x, 1, in_d, d_1x1)
+    conv3 = conv_relu_layer(x, 1, in_d, d_1x1)
+    maxpool = maxpool_layer(x, 3)
+    conv_maxpool = conv_layer(maxpool, 1, in_d, out_d)
+    conv3x3 = conv_layer(conv2, 3, d_1x1, int(out_d/2))
+    conv3x3 = conv_layer(conv3x3, 1, int(out_d/2), out_d)
+    conv5x5 = conv_layer(conv3, 5, d_1x1, int(out_d/2))
+    conv5x5 = conv_layer(conv5x5, 1, int(out_d/2), out_d)
+    return tf.nn.relu(tf.concat([conv1x1, conv3x3, conv5x5, conv_maxpool], 3))
+
+def Net(x, out=43):
+    conv1 = conv_relu_layer(x, 1, 3, 3) #26x26x3
+    conv2 = conv_relu_layer(conv1, 5, 3, 64) #26x26x32
+    incept1 = inception_module(conv2, 64, d_1) #26x26x256
+    maxpool1 = maxpool_stride_layer(incept1, 2, 2) #13x13x256
+    incept2 = inception_module(maxpool1, d_1*4, d_2) #13x13x512
+    maxpool2 = maxpool_stride_layer(incept2, 3, 2) #6x6x512
+    conv3 = conv_layer(maxpool2, 1, 512, 256) #6x6x256
+    fc0 = flatten(conv3)
+    fc1 = fc_layer(fc0, 256*6*6, 512)
+    fc1 = tf.nn.dropout(fc1, keep_prob)
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(512, out), mean = mu, stddev = sigma))
+    fc2_b  = tf.Variable(tf.zeros(out))
+    logits = tf.matmul(fc1, fc2_W) + fc2_b
+    return logits
+
+# Initialize tensorflow variables here
+EPOCHS = 5
+BATCH_SIZE = 128
+x = tf.placeholder(tf.float32, (None, 26, 26, 3))
+y = tf.placeholder(tf.int32, (None))
+keep_prob = tf.placeholder(tf.float32)
+one_hot_y = tf.one_hot(y, n_classes)
+
+logits = Net(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = one_hot_y)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+### Training pipeline
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+saver = tf.train.Saver()
+learning_curve = []
+
+def evaluate(X_data, y_data, sess):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        learning_curve.append(accuracy )
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+"""
+Model trainning:
+
+Run the training data through the training pipeline to train the model.
+Before each epoch, shuffle the training set. After each epoch, measure the loss
+and accuracy of the validation set. Save the model after training.
+
+"""
+# data preprocessing
+def eq_Hist(img):
+    #Histogram Equalization
+    img2=img.copy()
+    img2[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
+    img2[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
+    img2[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+    return img2
+
+def scale_img(img):
+    img2=img.copy()
+    sc_y=0.4*np.random.rand()+1.0
+    img2=cv2.resize(img, None, fx=1, fy=sc_y, interpolation = cv2.INTER_CUBIC)
+    c_x,c_y, sh = int(img2.shape[0]/2), int(img2.shape[1]/2), int(img_size/2)
+    return img2
+
+def crop(img, mar=0):
+    c_x,c_y, sh = int(img.shape[0]/2), int(img.shape[1]/2), int(img_size/2-mar)
+    return img[(c_x-sh):(c_x+sh),(c_y-sh):(c_y+sh)]
+
+def rotate_img(img):
+    c_x,c_y = int(img.shape[0]/2), int(img.shape[1]/2)
+    ang = 30.0*np.random.rand()-15
+    Mat = cv2.getRotationMatrix2D((c_x, c_y), ang, 1.0)
+    return cv2.warpAffine(img, Mat, img.shape[:2])
+
+def sharpen_img(img):
+    gb = cv2.GaussianBlur(img, (5,5), 20.0)
+    return cv2.addWeighted(img, 2, gb, -1, 0)
+#Compute linear image transformation ing*s+m
+def lin_img(img,s=1.0,m=0.0):
+    img2=cv2.multiply(img, np.array([s]))
+    return cv2.add(img2, np.array([m]))
+
+#Change image contrast; s>1 - increase
+def contr_img(img, s=1.0):
+    m=127.0*(1.0-s)
+    return lin_img(img, s, m)
+
+def transform_img(img):
+    img2=sharpen_img(img)
+    img2=crop(img2,3)
+    img2=contr_img(img2, 1.5)
+    return eq_Hist(img2)
+
+def augment_img(img):
+    img=contr_img(img, 1.8*np.random.rand()+0.2)
+    img=rotate_img(img)
+    img=scale_img(img)
+    return transform_img(img)
+
+X_train_trans = []
+X_val_trans = []
+X_test_trans = []
+
+def img_process():
+    for i in range(n_train):
+        X_train_trans.append(transform_img(X_train[i]))
+
+    for j in range(n_val):
+        X_val_trans.append(X_val[j])
+
+    print("Total number of images in the nex traing dataset = ", len(X_train_trans))
+    print("shape of the cropped data = ", X_train_trans[0].shape, "validation shape ",X_val_trans[0].shape)
+
+img_process()
+
+#
+start_time = time.time()
+with tf.Session() as sess:
+    with open('./t_log.csv', 'wt') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(['Epoch','step','lr','loss','acc'])
+        csvfile.flush()
+        sess.run(tf.global_variables_initializer())
+        num_examples = len(X_train)
+        print("Training...")
+        for i in range(EPOCHS):
+            print("EPOCH {} ".format(i+1))
+            X_train_aug, y_train_aug,  = shuffle(X_train_crop, y_train)
+            for offset in range(0, num_examples, BATCH_SIZE):
+                end = offset + BATCH_SIZE
+                batch_x, batch_y= X_train_aug[offset:end], y_train_aug[offset:end]
+                tr, loss = sess.run([training_operation, loss_operation], feed_dict={x: batch_x, y: batch_y,  keep_prob: dropout})
+                if offset%102400==0:
+                    acc = evaluate(X_val_crop,  y_val, sess)
+                    print(offset, loss, acc)
+                    writer.writerow([i+1, offset, rate, loss, acc])
+                    csvfile.flush()
+            print("Time :", time.time() - start_time)
+            start_time = time.time()
+            if (i+1)%5==0:
+                rate=rate*0.8
+    saver.save(sess, 'checkpoints/model_1.ckpt')
+    print("Model saved")
+
+# Tetsting the model
+#Prepare testing dataset
+X_test_prep=[]
+for i in range(n_test):
+    img=X_test[i]
+    X_test_prep.append(transform_img(img))
+
+#Run testing
+with tf.Session() as sess:
+    saver.restore(sess, 'checkpoints/model_2.1.ckpt')
+    test_accuracy = evaluate(X_test_prep, y_test, sess)
+    print("Test Accuracy = {:.3f}".format(test_accuracy))
